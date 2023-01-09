808d69f6f21b7df9eff5826953fe28d8dae72f1b
Carl Jia <jialia@gmail.com>
2023-01-09 17:49:54 +1100

    day 16 part 2

--- day16-b.pl
+++ day16-b.pl
@@ -2,7 +2,7 @@
 use v5.30;
 use warnings;
 use strict;
-use List::Util qw(first sum0 all pairs product min max);
+use List::Util qw(first sum0 all pairs product min max any);
 
 use Graph;
 
@@ -28,76 +28,48 @@ sub rate {
 
 my $max_points = 0;
 
-my $num_openable = grep { rate($_) } $graph->vertices;
+# shortest path between non-zero flow valves
+my $apsp = $graph->APSP_Floyd_Warshall;
 
-my %results; # to cache intermediate result
+# max points per combination of open valves
+# key is the sorted list of open valves
+my %max_open;
+my @flow_vs = grep { rate($_) } $graph->vertices;
 
-sub my_key {
-    my ($ha, $hb, $open, $time) = @_;
-    ($ha, $hb) = ($hb, $ha) if $ha gt $hb;
-    my $key = "$ha$hb" . join(".", sort {$a cmp $b} keys %$open) . $time;
-    return $key;
+# head, open, time limit, points
+my @queue = (["AA", {} , 26, 0]);
+while (my $state = shift @queue) {
+    my ($hd, $open, $remain, $pts) = @$state;
+    next if $remain < 0;
+
+    # update %max_open
+    my $key = join(",", sort {$a cmp $b} keys %$open);
+    $max_open{$key} = $pts if !$max_open{$key} || $max_open{$key} < $pts;
+
+    next if %$open == @flow_vs || $remain <= 0;
+
+    for my $v (grep {!exists $open->{$_}} @flow_vs) {
+        my $distance = $apsp->path_length($hd, $v);
+
+        # open next
+        my %noa = (%$open, $v => 1);
+        my $nremain = $remain - $distance - 1;
+        my $npts = $pts + $nremain * rate($v);
+        push @queue, [$v, \%noa, $nremain, $npts];
+    }
 }
 
-sub find_path {
-    my ($ha, $hb, $open, $time) = @_;
+for my $human (keys %max_open) {
+    my %open = map { $_ => 1 } split /,/, $human;
+    next if scalar %open == @flow_vs;
 
-    my $key = my_key(@_);
-    return $results{$key} if exists $results{$key};
+    for my $elephant (keys %max_open) {
+        my @open_e = split /,/, $elephant;
+        next if any { exists $open{$_} } @open_e;
 
-    # two conditions to quit
-    # 1. all nodes are visited
-    # 2. 26 mins of time is used
-    if (%$open == $num_openable || $time >= 25) {
-        return 0;
+        my $max = $max_open{$human} + $max_open{$elephant};
+        $max_points = $max if $max > $max_points;
     }
-
-    # bail out
-    return 0 if !%$open && $time > 3;
-
-    my @results;
-
-    my $ha_openable;
-    my $ratea = rate($ha);
-    if ($ratea > 0 && !exists $open->{$ha}) {
-        $ha_openable = 1;
-        my %noa = (%$open, $ha => 1);
-        # open a, move b
-        for my $vb ( $graph->neighbors($hb) ) {
-            push @results, (26 - $time - 1) * $ratea + find_path($ha, $vb, \%noa, $time+1);
-        }
-    }
-
-    my $rateb = rate($hb);
-    if ($rateb > 0 && !exists $open->{$hb}) {
-        my %nob = (%$open, $hb => 1);
-        # open b, move a
-        for my $va ( $graph->neighbors($ha) ) {
-            push @results, (26 - $time - 1) * $rateb + find_path($va, $hb, \%nob, $time+1);
-        }
-
-        # open a, open b
-        my %noboth = (%$open, $hb => 1, $ha => 1);
-        if (%noboth - %$open == 2) {
-            push @results, (26 - $time - 1) * ($ratea + $rateb) + find_path($ha, $hb, \%noboth, $time+1);
-        }
-    }
-
-    # move a, move b
-    for my $va ( $graph->neighbors($ha) ) {
-        for my $vb ( $graph->neighbors($hb) ) {
-            unless ($ha eq $hb && $va eq $vb) { # prevent copy
-                push @results, find_path($va, $vb, $open, $time+1);
-            }
-        }
-    }
-
-    return 0 unless @results;
-
-    my $rt = $results{$key} = max @results;
-    return $rt;
 }
-
-$max_points = find_path("AA", "AA", {}, 0);
 
 say $max_points;
